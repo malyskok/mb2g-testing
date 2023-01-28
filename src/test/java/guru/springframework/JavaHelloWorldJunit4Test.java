d319f9e81661b5636c2296ecf3ddf35b01df591a
malyskok <kamil.malysko@gmail.com>
2023-01-28 09:09:55 +0100

    groovy with spock and junit 4 and 5 (spock fails, 5 fails)

---
+++ src/test/java/guru/springframework/JavaHelloWorldJunit4Test.java
@@ -0,0 +1,13 @@
+package guru.springframework;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class JavaHelloWorldJunit4Test {
+
+    @Test
+    public void getHello() {
+        assertTrue(true);
+    }
+}
---
+++ src/test/java/guru/springframework/JavaHelloWorldJunit5Test.java
@@ -0,0 +1,13 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class JavaHelloWorldJunit5Test {
+
+    @Test
+    void getHello() {
+        assertTrue(true);
+    }
+}
