1f6086ee7644af5fd6845ca44066002b2347049c
malyskok <kamil.malysko@gmail.com>
2023-01-28 10:01:48 +0100

    failsafe IT test

---
+++ src/test/java/guru/springframework/JavaHelloWorldIT.java
@@ -0,0 +1,12 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class JavaHelloWorldIT {
+    @Test
+    void test() {
+        assertTrue(true);
+    }
+}
